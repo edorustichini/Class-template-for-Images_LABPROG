07b2fc81eab4518b590c7d0f308a83528402004b
edorustichini <edorusti@gmail.com>
2025-02-24 12:44:13 +0100

    Added write_to_PPM

--- src/Image.h
+++ src/Image.h
@@ -5,7 +5,8 @@
 
 #ifndef IMAGES_TEMPLATE__LABPROGR__IMAGE_H
 #define IMAGES_TEMPLATE__LABPROGR__IMAGE_H
-
+#include <fstream>
+#include <iostream>
 #include <vector>
 #include <stdexcept>
 
@@ -31,32 +32,66 @@ public:
     Image& operator=(const Image& img){
     }
 
-    int get_width(){
-        return width;
-    }
-    int get_height(){
-        return height;
-    }
+    int get_width(){ return width; }
+    int get_height(){ return height; }
 
-    void isValidIndex(int x, int y){
+    void is_valid_index(int x, int y) const{
         if (x < 0 || x >= width || y < 0 || y >= height) {
             throw std::out_of_range("Pixel index out of bounds");
         }
     }
 
-    Pixel& getPixel(int x, int y) {
-        isValidIndex(x, y);
+    Pixel& get_pixel(int x, int y) {
+        is_valid_index(x, y);
         int index = y * width + x;
         return pixels[index];
     }
 
-    void setPixel(int x, int y, const Pixel px) {
-        isValidIndex(x, y);
+    void set_pixel(int x, int y, const Pixel px) {
+        is_valid_index(x, y);
         int index = y * width + x;
         for (int i = 0; i < C; i++) {
             pixels[index].channels[i] = px.channels[i];
         }
     }
+
+    std::string get_magic_number(){
+        std::string magic_number;
+        // TODO: per ora gestito solo formato testuale, gestire anche binario?
+        if(C==1){
+            magic_number = "P2";
+        }
+        if(C==3){
+            magic_number = "P3";
+        }
+        else{
+            throw std::runtime_error("Formato PPM non supportato per C=" + std::to_string(C));
+        }
+        return magic_number;
+    }
+    void write_to_PPM(const std::string& filename) const{
+        std::ofstream img_file(filename, std::ios::out);
+        if(!img_file.is_open()){
+            throw std::runtime_error("bbbb");
+        }
+        std::string magic_number = get_magic_number();
+        img_file<< magic_number << "\n" << width << " " << height << 255<< "\n"; // FIXME: CREDO CHE  255 non abbia senso
+        //TODO: scrivere i valori dei pixel
+        for(Pixel px:pixels){
+            for(T val:px.channels){
+                img_file << val << " ";
+            }
+            img_file << "\n";
+        }
+        img_file.close();
+    }
+
+    /*TODO: un possibile test è: leggere un file già in PPM e creare l'oggetto Image equivalente, poi trasformalo con il metodo creato
+     * in un PPM e vedere se sono uguali*/
+
+
+
+
 
 
 
