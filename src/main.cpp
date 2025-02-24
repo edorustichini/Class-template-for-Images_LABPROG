c01ab5ebfc4d9e79a0634e60fb34cba578b2aea1
edorustichini <edorusti@gmail.com>
2025-02-24 12:47:29 +0100

    Added write_to_PPM

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "fstream"
+#include "Image.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+
     return 0;
+}
+
+
+template<typename T, int C>
+Image<T, C> readPPM(const std::string& filename){
+
 }
