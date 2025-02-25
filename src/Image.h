bd2613ce8f4a653fbae608dde5ced1f4bbd1c670
edorustichini <edorusti@gmail.com>
2025-02-25 15:13:43 +0100

    General refactor

--- src/Image.h
+++ src/Image.h
@@ -2,14 +2,12 @@
 // Created by edo on 23/02/25.
 //
 
-
 #ifndef IMAGES_TEMPLATE__LABPROGR__IMAGE_H
 #define IMAGES_TEMPLATE__LABPROGR__IMAGE_H
 #include <fstream>
 #include <iostream>
 #include <vector>
 #include <stdexcept>
-
 
 template<typename T, int C>
 struct Pixel{
@@ -20,7 +18,7 @@ template <typename T, int C>
 class Image {
 private:
     int width, height;
-    std::vector<Pixel<T,C>> pixels;//FIXME: forse da mettere pubblico, però se fatto, da cambiare anche altri metodi (readPPM)
+    std::vector<Pixel<T,C>> pixels; //FIXME: forse fa mettere privbato, ma nel caso cambiare funzione write/read(??)
 
 public:
 
@@ -29,40 +27,44 @@ public:
     }
 
     Image(const Image& other): width(other.width), height(other.height), pixels(other.pixels) {}
-    //TODO:operatore di asssegnazione
-    Image& operator=(const Image& img){
 
+    Image& operator=(const Image& img){
+        if (this != &img) {
+            width = img.width;
+            height = img.height;
+            pixels = img.pixels;
+        }
+        return *this;
     }
+
     ~Image()= default;
 
-    int get_width(){ return width; }
-    int get_height(){ return height; }
+    int get_width() const { return width; }
+    int get_height() const { return height; }
     void setWidth(int w) { width = w; }
     void setHeight(int h) { height = h; }
 
-    void is_valid_index(int x, int y) const{
+    void is_valid_index(int x, int y) const {
         if (x < 0 || x >= width || y < 0 || y >= height) {
-            throw std::out_of_range("Pixel index out of bounds");
+            throw std::out_of_range("Index out of bounds");
         }
     }
 
-
-    //FIXME: probabilmente da levare perché si possono accedere tramire vector
     Pixel<T,C>& get_pixel(int x, int y) {
         is_valid_index(x, y);
         return pixels[y * width + x];
     }
 
-    //FIXME: ha senso, forse sì per copy constructor o operatore di assegnazione
-    void set_pixel(int x, int y, const Pixel<T,C> px) {
+    void set_pixel(int x, int y, const Pixel<T,C>& px) {
         is_valid_index(x, y);
         int index = y * width + x;
         for (int i = 0; i < C; i++) {
             pixels[index].channels[i] = px.channels[i];
         }
     }
-    //avendo i valori per ogni canale
-    void set_pixel(int x, int y, T values[C]){
+
+    //se ho valori per ogni canale
+    void set_pixel(int x, int y, const T values[C]){
         is_valid_index(x, y);
         int index = y * width + x;
         for(int i=0; i < C; i++){
@@ -70,14 +72,12 @@ public:
         }
     }
 
-    std::string get_magic_number(){
+    std::string get_magic_number() const {
         std::string magic_number;
-        // TODO:  gestire anche 2, 4 CANALI
-        //TODO: per ora gestita solo forma testuale, gestire anche forma binaria?
         if(C==1){
             magic_number = "P2";
         }
-        if(C==3){
+        else if(C==3){
             magic_number = "P3";
         }
         else{
@@ -85,29 +85,26 @@ public:
         }
         return magic_number;
     }
-    void write_to_PPM(const std::string& filename) {
-        std::ofstream img_file(filename, std::ios::app);
+
+    void write_to_PPM(const std::string& filename) const {
+        std::ofstream img_file("../images/output/"+filename, std::ios::out);
         if(!img_file.is_open()){
-            throw std::runtime_error("Error");
+            throw std::runtime_error("Error opening file");
         }
         std::string magic_number = get_magic_number();
-        int max_val = 255; //FIXME: da definire per ogni canale?
+        int max_val = 255; //valore per formati PPM
 
-        //ppm header
-        img_file<< magic_number << "\n" << width << " " << height << "\n"<< max_val<< "\n";
+        //reading of header
+        img_file << magic_number << "\n" << width << " " << height << "\n" << max_val << "\n";
 
-        //pixels values
-        for (const Pixel px : pixels) {
+        //reading of pixels
+        for (const auto& px : pixels) {
             for (int i = 0; i < C; i++) {
-                img_file << static_cast<int>(px.channels[i]) << " ";  //Guarantee PPM correct format
+                img_file << static_cast<int>(px.channels[i]) << " ";
             }
-            img_file << "\n";//FIXME?: forse da cambiare per risparmiare spazio?
+            img_file << "\n";
         }
         img_file.close();
     }
-
-    /*TODO: un possibile test è: leggere un file già in PPM e creare l'oggetto Image equivalente, poi trasformalo con il metodo creato
-     * in un PPM e vedere se sono uguali*/
 };
-
 #endif //IMAGES_TEMPLATE__LABPROGR__IMAGE_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
-#include "fstream"
+#include <fstream>
+#include <cstdint>
 #include "Image.h"
+#include "PPM_IO.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    try {
 
+        Image<int, 3> img = PPM_IO::read_PPM<int, 3>("../images/skylight.ppm");
+
+        std::cout << "L'immagine è stata letta con successo!" << std::endl;
+        std::cout << "Larghezza: " << img.get_width() << " Altezza: " << img.get_height() << std::endl;
+
+
+        PPM_IO::write_to_PPM("landscape.ppm", img);
+        std::cout << "Immagine scritta su skylight.ppm" << std::endl;
+
+    } catch (const std::exception& e) {
+        std::cout << "Errore: " << e.what() << std::endl;
+    }
 
     return 0;
 }
 
 
-template<typename T, int C>
-Image<T, C> readPPM(const std::string& filename){
 
-}
