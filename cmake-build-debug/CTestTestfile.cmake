c144954ff97f29fed4aea0531c89eb8da7e20142
edorustichini <edorusti@gmail.com>
2025-02-28 10:21:22 +0100

    Aggiornato .gitignore e levata cmake-build/

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/edo/CLionProjects/Images_Template_(LabProgr)
-# Build directory: /home/edo/CLionProjects/Images_Template_(LabProgr)/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
