dfc71124e12e5db29c201477d090027019c054c3
edorustichini <edorusti@gmail.com>
2025-03-06 17:28:30 +0100

    Removed get_channels method

--- src/PPM_IO.cpp
+++ src/PPM_IO.cpp
@@ -3,24 +3,3 @@
 //
 
 #include "PPM_IO.h"
-//TODO: capire dove è usata
-int PPM_IO::determine_channels(const std::string &filename) {
-    std::ifstream img_file(filename, std::ios::in);
-    if (!img_file.is_open()) {
-        throw std::runtime_error("Impossibile aprire il file.");
-    }
-
-    std::string magic_number;
-    img_file >> magic_number;
-    img_file.close();
-
-    int nch = 0;
-    if (magic_number == "P3") {
-        nch = 3;
-    } else if (magic_number == "P2") {
-        nch = 1;
-    } else {
-        throw std::runtime_error("Formato file non supportato: " + magic_number);
-    }
-    return nch;
-}
