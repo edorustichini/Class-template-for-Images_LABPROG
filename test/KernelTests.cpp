b944ec580f504ae2fe82f689f468661bed43b071
edo <edorusti@gmail.com>
2025-03-10 11:55:09 +0100

    Setup gtest

---
+++ test/KernelTests.cpp
@@ -0,0 +1,6 @@
+//
+// Created by edo on 10/03/25.
+//
+
+#include <gtest/gtest.h>
+#include "../src/Image.h"
---
+++ test/runAllTests.cpp
@@ -0,0 +1,9 @@
+//
+// Created by edo on 10/03/25.
+//
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
